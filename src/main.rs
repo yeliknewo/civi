5342c19dc2e73acf4637cc8a4b7917dab2e19277
Kiley Owen <yelikdev@gmail.com>
2016-07-31 22:58:56 -0400

    Don't remember

--- src/main.rs
+++ src/main.rs
@@ -40,18 +40,18 @@ gfx_defines! {
 //     Vertex { pos: [  0.5, -0.5, -5.0, 1.0 ], color: [0.0, 1.0, 0.0] },
 //     Vertex { pos: [  0.0,  0.5, -5.0, 1.0 ], color: [0.0, 0.0, 1.0] }
 // ];
-
-const SQUARE_VERTEX_DATA: [Vertex; 4] = [
-    Vertex { pos: [ -0.5, -0.5, 0.0, 1.0 ], color: [1.0, 0.0, 0.0] },
-    Vertex { pos: [  0.5, -0.5, 0.0, 1.0 ], color: [0.0, 1.0, 0.0] },
-    Vertex { pos: [ -0.5,  0.5, 0.0, 1.0 ], color: [0.0, 0.0, 1.0] },
-    Vertex { pos: [  0.5,  0.5, 0.0, 1.0 ], color: [0.0, 0.0, 0.0] }
-];
-
-const SQUARE_INDEX_DATA: &'static [u16] = &[
-    0, 1, 3,
-    3, 2, 0,
-];
+//
+// const SQUARE_VERTEX_DATA: [Vertex; 4] = [
+//     Vertex { pos: [ -0.5, -0.5, 0.0, 1.0 ], color: [1.0, 0.0, 0.0] },
+//     Vertex { pos: [  0.5, -0.5, 0.0, 1.0 ], color: [0.0, 1.0, 0.0] },
+//     Vertex { pos: [ -0.5,  0.5, 0.0, 1.0 ], color: [0.0, 0.0, 1.0] },
+//     Vertex { pos: [  0.5,  0.5, 0.0, 1.0 ], color: [0.0, 0.0, 0.0] }
+// ];
+//
+// const SQUARE_INDEX_DATA: &'static [u16] = &[
+//     0, 1, 3,
+//     3, 2, 0,
+// ];
 
 const CLEAR_COLOR: [f32; 4] = [0.1, 0.2, 0.3, 1.0];
 
